6cb3b1f320230f08e3de34ac97cc2326443130ed
xylotope <daniels1924@gmail.com>
2017-05-30 14:10:20 -0600

    Query for RPM added.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $(function () {
     "use strict";
     var rpmTimer = setInterval(function () {
-        $.ajax({url: "stats/rpm.txt", success: function (result) {
-            $("car-stat1").html = result;
+        $.ajax({url: "stats/rpm.txt", dataType: "text", success: function (result) {
+            $("#car-stat1").html(result);
         }});
     }, 1000);
 });
