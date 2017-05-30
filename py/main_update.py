d7516c0c137c65fd5e894bb83ac19e0d1ffe0c35
xylotope <daniels1924@gmail.com>
2017-05-30 16:12:34 -0600

    Added python folder.

---
+++ py/main_update.py
@@ -0,0 +1,37 @@
+import signal
+import sys
+import time
+import obd
+from obd import OBDStatus
+
+def sigterm_handler(signal, frame):
+    sys.exit(0)
+
+signal.signal(signal.SIGTERM, sigterm_handler)
+
+def connect():
+    connection = obd.OBD()
+    print connection.status()
+    while connection.status() != OBDStatus.CAR_CONNECTED:
+        connection = obd.OBD()
+        print connection.status()
+        time.sleep(5)
+    return connection
+
+if __name__ == "__main__":
+    try:
+        while True:
+            connection = connect()
+            while True:
+                r = connection.supports(obd.commands.RPM)
+                if r == False:
+                    with open("../stats/rpm.txt", "w") as f:
+                        f.write("Off")
+                    connect()
+                else:
+                    with open("../stats/rpm.txt", "w") as f:
+                        f.write(r)
+                time.sleep(1)
+    except KeyboardInterrupt:
+        print "Good bye"
+        connection.close()
